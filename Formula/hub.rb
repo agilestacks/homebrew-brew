cb70bbb9c0a033a8b9bd8fc16aa18833cd66ad91
Arkadi Shishlov <arkadi@agilestacks.com>
2020-12-09 18:56:07 +0200

    Bump version, `hub` binary hash

--- Formula/hub.rb
+++ Formula/hub.rb
@@ -2,10 +2,10 @@ class Hub < Formula
     desc "Hub CLI is stack composition and lifecycle tool"
     homepage "https://superhub.io/"
     url "https://controlplane.agilestacks.io/dist/hub-cli/hub.darwin_amd64"
-    version "2020-11-24"
+    version "2020-12-09"
     license "GPLv3"
     head "https://github.com/agilestacks/hub"
-    sha256 "20d7b0f3c5d48d7ee3372813622bfff7f8cb6ed9d4220327f6dac4b9bec54ac9"
+    sha256 "75ebf363e7b18b773a443245e17d1751819cbcb7f5acc5a3480a4c9df11244b3"
 
 
     depends_on "jq"
