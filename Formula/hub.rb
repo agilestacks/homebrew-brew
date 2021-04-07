fc27fa8a08b343131a59c9ee52286911e9b8e3bc
Arkadi Shishlov <arkadi@agilestacks.com>
2021-04-07 20:21:36 +0300

    `hub` binary v1.0.0

--- Formula/hub.rb
+++ Formula/hub.rb
@@ -1,14 +1,15 @@
 class Hub < Formula
     desc "Hub CLI is stack composition and lifecycle tool"
     homepage "https://superhub.io/"
-    url "https://controlplane.agilestacks.io/dist/hub-cli/hub.darwin_amd64"
-    version "2020-12-09"
+    url "https://github.com/agilestacks/hub/releases/download/v1.0.0/hub.darwin_amd64"
+    version "v1.0.0"
     license "GPLv3"
     head "https://github.com/agilestacks/hub"
-    sha256 "75df638332896ca7c48833f623348d2e5a002c08681857aac78c967a73d6bb35"
+    sha256 "826167675e7a7f2cbde159a44c90b6b8175de5b7e2f3bbd221e07223ea5fad96"
 
 
     depends_on "jq"
+    depends_on "yq"
     depends_on "npm"
 
 
